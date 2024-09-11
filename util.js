1d281202dc3c135fc584c962fa5dfb5c70baf273
trifles <132377611+unconsideredtrifles@users.noreply.github.com>
2024-09-11 18:28:25 +0630

    Change square comparing function name
    Gave it a more sensible name.

--- util.js
+++ util.js
@@ -16,12 +16,12 @@ const convertVertixIdxToSquare = function convertVertixIdxToSquare(
   return [row, col]
 };
 
-const compareSquares = function compareSquares(square1, square2) {
+const isSameSquare = function compareSquares(square1, square2) {
   return (square1[0] === square2[0] && square1[1] === square2[1])
 };
 
 export {
   convertSquareToVertixIdx,
   convertVertixIdxToSquare,
-  compareSquares,
+  isSameSquare,
 };
